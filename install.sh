ef6a055feddc75453262ad21d95b35e1db5a8bba
djokavac-ops <djokavac@gmail.com>
2026-06-30 20:55:17 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-echo "Run: uvicorn server:app --host 0.0.0.0 --port 8000"
